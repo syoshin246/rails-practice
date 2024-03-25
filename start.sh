555f05f77c5d591dde470be3d920e7d69d58a35a
tachibana <sapurizemi@icloud.com>
2024-03-26 08:54:32 +0900

    本番用に

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin\sh
 
-# if[ "${RAILS_ENV}" = "prodction" ]
-# then
-#     bundle exec rails assets:precompile
-# fi
-
-# bundle exec rails s -p ${PORT:-3000} -b 0.0.0.0
+if[ "${RAILS_ENV}" = "prodction" ]
+then
+# 本番に必要な処理
+    bundle exec rails assets:precompile
+fi
+# Railsサーバーを立ち上げる
+bundle exec rails s -p ${PORT:-3000} -b 0.0.0.0
