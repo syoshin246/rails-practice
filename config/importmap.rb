a5b3d3a833f4140bfbc9fa87be7836c65823310b
tachibana <sapurizemi@icloud.com>
2024-04-07 11:47:27 +0900

    aa

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,14 @@
 pin "application", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "popper", to: "popper.js", preload: true
+
+module Importmap
+  def self.assets
+    {
+      "application" => {
+        "application" => "app/assets/stylesheets/application.css",
+        "application_js" => "app/assets/javascripts/application.js"
+      }
+    }
+  end
+end
